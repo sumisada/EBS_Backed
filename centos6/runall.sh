0af4d93bbe5824c1ea21377f810cfa8b00a61cbc
sumisada <s43@mac.com>
2015-01-17 00:25:33 +0900

    run all step

---
+++ centos6/runall.sh
@@ -0,0 +1,14 @@
+# runall step to build CentOS6.5 HVM AMI
+#
+#
+#
+
+./setup1_partformat.sh
+./setup2_preinstall.sh
+./setup3_grubconf.sh
+./setup4_network.sh
+./setup5_final.sh
+
+#
+# End Of Script...
+#
